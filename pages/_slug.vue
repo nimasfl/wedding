8fa5c5922b6d7c368e0b2aed268abcaf620c05df
nima soufiloo <nima.soufiloo@gmail.com>
2022-03-29 20:49:23 +0430

    almost completed

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -2,9 +2,18 @@
   <div class="bg-[#EEF5F0] h-screen flex justify-center" @click="clicked">
     <audio id="music" src="/love-story.mp3" loop="loop" autoplay></audio>
     <div class="image1 w-screen md:w-[540px]" :class="animate && 'slide-out'">
+      <div
+        class="absolute left-0 right-0 top-0 mt-12 opacity-0"
+        :class="showClick && 'appear'"
+      >
+        <p class="text-center text-[2.5rem] font-bold">کلیک کنید</p>
+      </div>
       <div class="bg-[#EEF5F0] h-8 w-full absolute bottom-0" />
       <div class="bg-[#EEF5F0] w-2 absolute bottom-0 top-0 left-0" />
       <div class="bg-[#EEF5F0] w-2 absolute bottom-0 top-0 right-0" />
+      <div class="absolute bottom-0 left-0 right-0 h-8 mb-8">
+        <p class="text-center font-medium text-2xl">{{ parse() }}</p>
+      </div>
     </div>
     <div class="image2 w-screen md:w-[540px] relative">
       <a
@@ -22,12 +31,29 @@ export default Vue.extend({
   name: 'IndexPage',
   data: () => ({
     animate: false,
-    play: false
+    play: false,
+    showClick: false,
+    timer: null as NodeJS.Timeout | null
   }),
+  mounted() {
+    this.timer = setTimeout(() => {
+      this.showClick = true
+    }, 2000)
+  },
   methods: {
+    parse() {
+      return decodeURI(this.$route.path.toString())
+        .trim()
+        .replace('/', '')
+        .replaceAll('_', ' ')
+    },
     clicked() {
       ;(document.getElementById('music') as HTMLAudioElement).play()
       this.animate = true
+      this.showClick = false
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
     }
   }
 })
@@ -44,6 +70,11 @@ export default Vue.extend({
   z-index: 3;
   transition: transform ease-out 7000ms, opacity ease-out 1000ms,
     box-shadow ease-out 500ms;
+}
+
+.appear {
+  opacity: 100%;
+  transition: opacity ease-in 1000ms;
 }
 
 .slide-out {
